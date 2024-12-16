e318f9189f24b2fac2c58185bfd336298fad4dca
Adrian Lopez <adrian.lopez788@comunidadunir.net>
2024-12-16 23:41:04 +0100

    20241216
    Acitvidad 1  completa

--- Assets/Scripts/Personaje.cs
+++ Assets/Scripts/Personaje.cs
@@ -4,32 +4,42 @@ using UnityEngine;
 
 public class Personaje : MonoBehaviour
 {
+    // Velocidades serializadas de giro y desplazamiento
+    [SerializeField] 
+    private float velocidadGiro = 100;
+    [SerializeField] 
+    private float velocidadDesplazamiento = 1;
 
-    private string nombre;
-    private int vidas;
-    private int edad;
-    private float danho;
-    private float defensa;
-
-    private void Mover(float x, float y, float z, float velocidad)
 
     // Start is called before the first frame update
     void Start()
-    {
-        Debug.Log("Hola Mundo");
-        SumarDosNumeros();
-    }
+    {}
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Ha pasado un frame");
-        
+        CharacterController controller = GetComponent<CharacterController>();
+        // Obtener valores de entrada del jugador
+
+        // Giro: teclas A/D o flechas izquierda/derecha
+        float inputHorizontal = Input.GetAxis("Horizontal"); 
+        // Movimiento: teclas W/S o flechas arriba/abajo
+        float inputVertical = -Input.GetAxis("Vertical");
+
+        // Rotar al personaje
+        transform.Rotate(0, inputHorizontal * velocidadGiro * Time.deltaTime, 0);
+
+        // Calcular la dirección de movimiento en espacio local
+        Vector3 direccionMovimiento = new Vector3(0, 0, inputVertical);
+
+        // Convertir la dirección al espacio global
+        direccionMovimiento = transform.TransformDirection(direccionMovimiento);
+
+        // Aplicar la velocidad de movimiento y el deltaTime
+        direccionMovimiento *= velocidadDesplazamiento * Time.deltaTime;
+
+        // Mover al personaje usando CharacterController
+        controller.Move(direccionMovimiento); ;
     }
 
-
-    void SumarDosNumeros() 
-    {
-        Debug.Log("Voy a sumnar dos numeros");
-    }
 }
